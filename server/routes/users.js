7eb0aa221618108e5e3dd78a401caaada421e758
IshaanMehta <ishaan2u@gmail.com>
2023-01-20 23:48:54 +0530

    user routes setup

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { verifyToken } from "../middleware/verifyToken";
+import {
+  getUser,
+  getUserFriend,
+  addRemoveFriend
+} from "../controllers/users.js";
+
+const router = express.Router();
+
+// READ ROUTES
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriend);
+
+// UPDATE ROUTES
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
