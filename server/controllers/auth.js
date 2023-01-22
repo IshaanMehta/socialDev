ee641ebc5a572ce8d5fc38a81bd6f3a6167b52e9
IshaanMehta <ishaan2u@gmail.com>
2023-01-22 23:19:31 +0530

    data enteries for the application

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -26,9 +26,9 @@ export const register = async (req, res) => {
       picturePath,
       friends,
       location,
-      occupation
-      // viewedProfile: Math.floor(Math.random * 1000),
-      // impressions: Math.floor(Math.random * 1000)
+      occupation,
+      viewedProfile: Math.floor(Math.random * 1000),
+      impressions: Math.floor(Math.random * 1000)
     });
 
     const savedUser = await newUser.save();
--- server/index.js
+++ server/index.js
@@ -10,7 +10,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import User from "./models/Users.js";
+import Post from "./models/Posts.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -40,10 +45,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
@@ -55,4 +62,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+
+    // RAW ENTRY DATA
+    User.insertMany(users);
+    Post.insertMany(posts);
   });
