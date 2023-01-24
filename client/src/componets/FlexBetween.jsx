7820de7b66ea517b83a7b36e7658d4ce8847db5e
IshaanMehta <ishaan2u@gmail.com>
2023-01-24 16:18:32 +0530

    reusable css  component

---
+++ client/src/componets/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/system";
+import { styled } from "@mui/material";
+
+const FlexBeteen = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center"
+});
+
+export default FlexBeteen;
