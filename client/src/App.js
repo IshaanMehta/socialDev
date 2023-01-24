503692d5a9ba702cfd3cfea39537f49951c85e84
IshaanMehta <ishaan2u@gmail.com>
2023-01-24 16:18:08 +0530

    Theme provider setup globally

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
-import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/system";
+import { themeSettings } from "./theme";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => themeSettings(mode), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
